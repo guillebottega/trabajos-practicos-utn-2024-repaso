8da815e62f5c65360a0524896c898beeb075e058
guillebottega <gbottega@hotmail.com>
2024-09-02 14:59:19 -0300

    subiendo archivos de los ejercicios clase 1

---
+++ src/EjercicioA.java
@@ -0,0 +1,39 @@
+
+
+public class EjercicioA {
+
+	public static void main(String[] args) {
+		int num1 =2;
+		int num2 =18;
+		
+		while(num1<=num2) {
+			System.out.print(num1+",");
+			num1=num1+1;
+			System.out.println(" ");
+		}
+		for (int i = 4; i <= 14; i++) {
+			System.out.println("los pares  son:"+i);
+			i++;		
+		} 	
+		 for (int i = 14; i >= 4; i--) {
+				System.out.println("los pares en forma decreciente son :"+i);
+				i--;		
+			} 		
+		 for (int i=6;i<=14;i++) {
+				 boolean par = false;
+				 if (par) {
+			 System.out.println("los numeros pares son: "+i);
+			 i++;
+			 } else {
+				 System.out.println("los impares son: "+(i+1));
+			 i++;}	}					 
+	    	 
+			
+	}}		
+	
+		
+		
+
+	
+
+
